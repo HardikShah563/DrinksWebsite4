a2c6fad06d1edea72667efbd5d84e082b774214a
HardikShah563 <hardikts@gmail.com>
2022-11-26 23:31:24 +0530

    add to cart added

---
+++ add-to-cart.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'head-links.php' ?>
+    <title>Drinksify</title>
+</head>
+
+<body>
+    <!-- Everything inside the file will be contained inside the box to get that effect -->
+    <div class="box all-pages">
+        <?php include 'navbar.php' ?>
+
+        <section class="about-us" id="about-us">
+            <div class="about-text">
+                <div class="main-text-pages">
+                    <h1><i class="fa-solid fa-circle-check"></i></h1>
+                    <h6>Product Added to Cart!</h6>
+                </div>
+            </div>
+            <div class="about-img">
+                <img src="img/four-combo.png" alt="Four Combo Img">
+            </div>
+        </section>
+
+        <hr>
+
+        <section class="footer">
+            <footer>
+                <p>&copy; Copyrights reserved | Drinksify 2022</p>
+                <div class="footer-links">
+                    <a href="#">Privacy Policy</a>
+                    <a href="#">Terms and Conditions</a>
+                </div>
+            </footer>
+        </section>
+    </div>
+</body>
+
+</html>
